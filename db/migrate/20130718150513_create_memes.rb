95c210fdd910e5b855e25ffaba1b11328a8b3f7c
Jay Chetty <jaypaulchetty@gmail.com>
2013-07-25 14:05:38 +0100

    memes remove correctly

--- db/migrate/20130718150513_create_memes.rb
+++ db/migrate/20130718150513_create_memes.rb
@@ -1,4 +1,3 @@
-class CreateMemes < ActiveRecord::Migration
   def change
     create_table :memes do |t|
       t.integer :user_id
@@ -6,6 +5,4 @@ class CreateMemes < ActiveRecord::Migration
 
       t.timestamps
     end
-    add_index  :memes, :user_id
   end
-end

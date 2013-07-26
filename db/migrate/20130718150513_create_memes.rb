be7e54855e61d26e9bb82c2e728dc80b8700e9fb
Jay Chetty <jaypaulchetty@gmail.com>
2013-07-26 16:39:54 +0100

    meme migration fixed

--- db/migrate/20130718150513_create_memes.rb
+++ db/migrate/20130718150513_create_memes.rb
@@ -1,8 +1,9 @@
+class CreateMemes < ActiveRecord::Migration
   def change
     create_table :memes do |t|
       t.integer :user_id
       t.integer :current_quote_id
 
       t.timestamps
-    end
   end
+end

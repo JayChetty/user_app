15c2a15aae24acda98274c1f7da7297a9949002e
Jay Chetty <jaypaulchetty@gmail.com>
2013-08-01 17:25:30 +0100

    removing files

--- app/models/item.rb
+++
@@ -1,6 +0,0 @@
-class Item < ActiveRecord::Base
-  attr_accessible :image_url, :url
-
-  belongs_to :shelf
-  has_one :read
-end
--- db/migrate/20130801114756_remove_image_url_url_shelf_id_from_reads.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImageUrlUrlShelfIdFromReads < ActiveRecord::Migration
-  def change
-  	remove_column :reads, :current_quote_id
-  end
-end

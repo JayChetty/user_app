30f77d146614860641869e79fdf9a80e9fddb886
Jay Chetty <jaypaulchetty@gmail.com>
2013-08-02 15:52:02 +0100

    feelings completed

--- app/controllers/feelings_controller.rb
+++ app/controllers/feelings_controller.rb
@@ -2,31 +2,53 @@ class FeelingsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @shelves = @user.shelves.all
-    @feelings = @user.feelings.all
+    @feelings = @user.feelings.all(:order => "created_at DESC")
+
+    #if want comments on 
+    # if params[:feeling_id]
+    #   @feeling = current_user.feelings.find(params[:feeling_id])
+    # end
   end
 
-  def new
-  	@user = User.find(params[:user_id])
-  	@shelves = current_user.shelves.all
-    @shelf = current_user.shelves.find(params[:shelf_id])
-    resource, id = request.path.split('/')[5, 6]
-    @feelable = resource.singularize.classify.constantize.find(id)
-    @feeling = current_user.feelings.new
-    flash.now[:success] = "feelable #{@feelable.class}"
-  end
+  # def new
+  # 	@user = User.find(params[:user_id])
+  # 	@shelves = current_user.shelves.all
+  #   @shelf = current_user.shelves.find(params[:shelf_id])
+  #   # resource, id = request.path.split('/')[5, 6]
+  #   # @feelable = resource.singularize.classify.constantize.find(id)
+  #   @feeling = current_user.feelings.new
+  #   flash.now[:success] = "feelable #{@feelable.class}"
+  # end
 
   def create
     resource, id = request.path.split('/')[5, 6]
-    flash.now[:success] = "#{params}"
+    #flash[:success] = "#{params}"
     @feeling = current_user.feelings.build(feelable_type: resource.singularize.classify, 
                                           feelable_id: id)
     if @feeling.save
       flash[:success] = "Feeling Created"
-      redirect_to user_path(current_user)
+      redirect_to user_feelings_path(current_user)
     else
       flash[:failure] = "Couldn't create feeling"
-      redirect_to user_path(current_user)
+      redirect_to user_feelings_path(current_user)
     end
-
   end
+
+  def destroy
+    @feeling = current_user.feelings.find(params[:id])
+    if @feeling.destroy
+      flash[:success] = "Feeling destroyed"
+    else
+      flash[:failure] = "Could not destroy Feeling"
+    end
+    redirect_to user_feelings_path(current_user)      
+  end
+
+  # def edit
+  #   #flash[:success] = " lala #{params}"
+  #   #redirect_to user_feelings_path(current_user)
+  #   @feeling = current_user.feelings.find(params[:id])
+  #   redirect_to user_feelings_path(current_user, @shelf, feeling_id: @feeling.id)
+  # end
+
 end

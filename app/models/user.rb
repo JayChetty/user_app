8b1ac0f7dead1a97a5b7ad41d1e9abef7ae3e4a2
Jay Chetty <jaypaulchetty@gmail.com>
2013-08-07 08:04:56 +0100

    dependentdestroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,7 @@ class User < ActiveRecord::Base
 
   has_many :friendships, dependent: :destroy
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id", dependent: :destroy
+  
   has_many :friends, through: :friendships, conditions: "friendships.status = 'confirmed'"
 
   has_many :sent_cards, foreign_key: "sender_id", class_name: "Card"

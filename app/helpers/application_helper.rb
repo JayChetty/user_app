af0055491f75c39578cf204bde93d0539028e117
Jay Chetty <jaypaulchetty@gmail.com>
2013-08-08 13:28:23 +0100

    tidied

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Shelves"
+    base_title = "Stirs"
     if page_title.empty?
       base_title
     else
